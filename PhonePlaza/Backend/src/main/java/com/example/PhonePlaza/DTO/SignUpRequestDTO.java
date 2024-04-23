865fe01bb9602b2e4338ce40fef979513f0249dd
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-23 23:10:53 +0530

    Create SignUpRequestDTO.java

---
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/DTO/SignUpRequestDTO.java
@@ -0,0 +1,10 @@
+package com.example.PhonePlaza.DTO;
+
+import lombok.Data;
+
+@Data
+public class SignUpRequestDTO {
+    private String userName;
+    private  String email;
+    private String password;
+}
