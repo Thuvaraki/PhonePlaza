09c09c124040b688a44a5ddbe25d5e5bcbfd6262
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-23 11:09:37 +0530

    Initial commit

---
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/PhonePlazaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhonePlaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonePlazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonePlazaApplication.class, args);
+	}
+
+}
---
+++ PhonePlaza/Backend/src/test/java/com/example/PhonePlaza/PhonePlazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhonePlaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonePlazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
