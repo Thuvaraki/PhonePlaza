af6f40cfcafb494b795dd62910943932779a28f6
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-24 13:40:54 +0530

    Update UserController.java

--- PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Controller/UserController.java
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.PhonePlaza.DTO.SignUpRequestDTO;
 import com.example.PhonePlaza.Service.UserService;
 import com.example.PhonePlaza.Util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,5 +39,31 @@ public class UserController {
         APIResponse apiResponse = userService.Login(LoginRequestDTO);
         return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
     }
+
+    @PostMapping(path = "/forgotPassword")
+    public ResponseEntity<APIResponse> ForgotPassword(@RequestParam String email){
+        APIResponse apiResponse = userService.ForgotPassword(email);
+        return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+    }
+
+    @PostMapping("/changePassword")
+    public ResponseEntity<APIResponse> ChangePassword(@RequestParam String email,@RequestParam String password){
+        System.out.println("email: "+email);
+        System.out.println("password: "+password);
+        APIResponse apiResponse = userService.ChangePassword(email,password  );
+
+        return ResponseEntity
+                .status(apiResponse.getStatus())
+                .body(apiResponse);
+    }
+
+    @PostMapping(path = "/logout")
+    public ResponseEntity<APIResponse> logout() {
+        APIResponse apiResponse = new APIResponse();
+        apiResponse.setStatus(HttpStatus.OK.value());
+        apiResponse.setData("Logged out successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
 
 }
