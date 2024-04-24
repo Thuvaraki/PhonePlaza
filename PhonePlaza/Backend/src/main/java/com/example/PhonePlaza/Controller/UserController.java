aee74669b3388cbf7b48174f1bd92773214538ed
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-24 11:47:00 +0530

    Update UserController.java

--- PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Controller/UserController.java
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.PhonePlaza.Controller;
 
 import com.example.PhonePlaza.Common.APIResponse;
+import com.example.PhonePlaza.DTO.LoginRequestDTO;
 import com.example.PhonePlaza.DTO.SignUpRequestDTO;
 import com.example.PhonePlaza.Service.UserService;
+import com.example.PhonePlaza.Util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,19 +16,27 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    JwtUtils jwtUtils;
+
     @PostMapping(path = "/signup")
     public ResponseEntity<APIResponse> signUp(@RequestBody SignUpRequestDTO signUpRequestDTO){
         APIResponse apiResponse = userService.signUp(signUpRequestDTO);
         return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
     }
-
-    @PostMapping("/verifyOtp/{email}/{otp}")
-    public ResponseEntity<APIResponse> verifyOTP(@PathVariable String email, @PathVariable String otp){
-        APIResponse apiResponse=userService.verifyOTP(email,otp);
+    @PostMapping("/emailVerificationUsingOTP/{email}/{otp}")
+    public ResponseEntity<APIResponse> EmailVerificationUsingOTP(@PathVariable String email, @PathVariable String otp){
+        APIResponse apiResponse=userService.EmailVerificationUsingOTP(email,otp);
 
         return ResponseEntity
                 .status(apiResponse.getStatus())
                 .body(apiResponse);
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<APIResponse> Login(@RequestBody LoginRequestDTO LoginRequestDTO){
+        APIResponse apiResponse = userService.Login(LoginRequestDTO);
+        return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
     }
 
 }
