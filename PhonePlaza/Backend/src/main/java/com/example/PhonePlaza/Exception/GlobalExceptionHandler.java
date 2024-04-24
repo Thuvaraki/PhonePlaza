046af9b6ed4ccb8d271eee9d652980acad0ca57f
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-24 21:19:33 +0530

    Update GlobalExceptionHandler.java

--- PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Exception/GlobalExceptionHandler.java
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Exception/GlobalExceptionHandler.java
@@ -9,9 +9,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
+    public ResponseEntity handleException(Exception e){
+
+        APIResponse apiResponse=new APIResponse();
+
+        apiResponse.setError("Oops Something went wrong");
+        apiResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(apiResponse);
+    }
+    @ExceptionHandler
     public ResponseEntity handleAccessDeniedException(AccessDeniedException e) {
         APIResponse apiResponse = new APIResponse();
         apiResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
         return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleProductNotFoundException(ProductNotFoundException e) {
+        APIResponse apiResponse = new APIResponse();
+        apiResponse.setError(e.getMessage());
+        apiResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(apiResponse);
     }
 }
