28f6e782774bbfac801282ff456a4b45e2cb3885
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-24 21:09:08 +0530

    Create Product.java

---
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Entity/Product.java
@@ -0,0 +1,29 @@
+package com.example.PhonePlaza.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @Column(name="product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productId;
+
+    @Column(name="product_name")
+    private String productName;
+
+    @Column(name="product_price")
+    private String price;
+
+    @Column(name="product_description")
+    private String description;
+
+    @Column(name="product_image")
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name="category_id",referencedColumnName = "category_id",nullable = false)
+    private Category category;
+}
