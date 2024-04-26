8ca6916363db67b2cf23b40d048a2e580f93937f
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-26 12:46:02 +0530

    Update UserRepository.java

--- PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Repository/UserRepository.java
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Repository/UserRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByEmail(String email);
 
-    void deleteByEmail(String email);
-
     User findOneByEmailIgnoreCaseAndPassword(String email, String password);
 }
