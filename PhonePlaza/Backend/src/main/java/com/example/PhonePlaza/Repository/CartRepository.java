5db7a565b61d08d4ef9c3c484239b8521fb39896
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-26 12:47:02 +0530

    Create CartRepository.java

---
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.example.PhonePlaza.Repository;
+
+import com.example.PhonePlaza.Entity.Cart;
+import com.example.PhonePlaza.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart,Integer> {
+    Cart findByUser(User user);
+}
