01c6f21a8a2eef0f1e50fc88214c786eb0c2be07
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-26 12:47:17 +0530

    Create CartController.java

---
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Controller/CartController.java
@@ -0,0 +1,22 @@
+package com.example.PhonePlaza.Controller;
+
+import com.example.PhonePlaza.Common.APIResponse;
+import com.example.PhonePlaza.DTO.CartItemRequestDTO;
+import com.example.PhonePlaza.Service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/phoneplaza/user/cart")
+public class CartController {
+        @Autowired
+        private CartService cartService;
+
+    @PostMapping("/add")
+    public ResponseEntity<APIResponse> addToCart(@RequestBody CartItemRequestDTO itemRequest, @RequestHeader(required = false) String authorizationHeader) {
+        APIResponse apiResponse = cartService.addItemToCart(itemRequest, authorizationHeader).getBody();
+        return ResponseEntity.status(HttpStatus.valueOf(apiResponse.getStatus())).body(apiResponse);
+    }
+}
