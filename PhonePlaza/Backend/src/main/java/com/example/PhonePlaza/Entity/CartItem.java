6a469d199943188ad961ac951aed823c1cec0238
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-26 12:45:49 +0530

    Update CartItem.java

--- PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Entity/CartItem.java
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Entity/CartItem.java
@@ -1,14 +1,18 @@
 package com.example.PhonePlaza.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-public class CartProduct {
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer CartItemId;
 
     @ManyToOne
     @JoinColumn(name="cartId",referencedColumnName = "cartId")
@@ -20,8 +24,12 @@ public class CartProduct {
 
     private Integer quantity;
 
-    private Integer subTotal;
+    private Double subTotal;
 
-
-
+    public CartItem(Cart cart, Product product, Integer quantity, Double subTotal) {
+        this.cart = cart;
+        this.product = product;
+        this.quantity = quantity;
+        this.subTotal = subTotal;
+    }
 }
