b4cc7bffd58a00d4b1b789a543b96b42c3001950
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-26 12:47:11 +0530

    Create CartItemRequestDTO.java

---
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/DTO/CartItemRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.PhonePlaza.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemRequestDTO {
+    private int productId;
+    private int quantity;
+}
