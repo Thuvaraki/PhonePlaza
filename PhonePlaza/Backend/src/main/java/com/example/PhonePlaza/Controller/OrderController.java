2d111a6d06fc70f0c694d7d6f1f8cbc8a3e003d9
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-27 23:00:04 +0530

    Create OrderController.java

---
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.example.PhonePlaza.Controller;
+
+import com.example.PhonePlaza.Common.APIResponse;
+import com.example.PhonePlaza.DTO.OrderRequestDTO;
+import com.example.PhonePlaza.ExceptionAndHandler.UserNotFoundException;
+import com.example.PhonePlaza.Service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/phoneplaza/user/order")
+public class OrderController {
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("/create")
+    public ResponseEntity<APIResponse> createOrder(@RequestBody OrderRequestDTO orderReq) throws UserNotFoundException {
+        return orderService.createOrder(orderReq);
+    }
+
+    @GetMapping("/getOrderItems")
+    public ResponseEntity<APIResponse> getOrders(@RequestParam String email) throws UserNotFoundException {
+        return orderService.getOrders(email);
+    }
+
+    @DeleteMapping("/deleteOrder/{orderId}")
+    public ResponseEntity<APIResponse> cancelOrderById(@PathVariable int orderId) {
+        return orderService.cancelOrder(orderId);
+    }
+}
