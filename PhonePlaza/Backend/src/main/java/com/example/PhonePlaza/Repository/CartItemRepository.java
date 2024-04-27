85d9183ac8544b7b56065aaad9be2e16f603540a
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-04-27 13:09:49 +0530

    Update CartItemRepository.java

--- PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Repository/CartItemRepository.java
+++ PhonePlaza/Backend/src/main/java/com/example/PhonePlaza/Repository/CartItemRepository.java
@@ -5,9 +5,13 @@ import com.example.PhonePlaza.Entity.CartItem;
 import com.example.PhonePlaza.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
 
     CartItem findByCartAndProduct(Cart cart, Product product);
+
+
+    List<CartItem> findAllByCart(Cart cart);
 }
